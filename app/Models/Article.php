b6e2b38f326fd344ac9b53e0b158ac63518bd252
carinhanerd <miliandroxd@gmail.com>
2021-09-29 23:42:22 -0300

    corrigido dependência desnecessária

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -5,12 +5,10 @@ namespace App\Models;
 use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Spatie\MediaLibrary\HasMedia;
-use Spatie\MediaLibrary\InteractsWithMedia;
 
-class Article extends Model implements HasMedia
+class Article extends Model
 {
-    use HasFactory, InteractsWithMedia;
+    use HasFactory;
 
     protected $guarded = [];
 
